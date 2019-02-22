9fa4bc8b8ab15fe253b48a7c635763796bb7372a
Antisida <sokolov.nnov@gmail.com>
2019-02-22 07:39:03 +0300

    перед мержем веев

--- src/main/java/ConnectivityTest.java
+++ src/main/java/ConnectivityTest.java
@@ -8,8 +8,8 @@ public class ConnectivityTest {
         MemoryStorage data1 = null;
         List<OsmWay> ss = new ArrayList<>();
         try {
-            // data = new O5MReader().read(new File("e:\\osmtmp\\RU-NIZ.o5m"));
-            data = new O5MReader().read(new File("c:\\osm\\RU-N_01.o5m"));
+             data = new O5MReader().read(new File("e:\\osmtmp\\RU-NIZ.o5m"));
+            //data = new O5MReader().read(new File("c:\\osm\\RU-N_01.o5m"));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -38,35 +38,81 @@ public class ConnectivityTest {
                //System.out.println(o.getTag("name", finalData));
             });
         }
+
+        System.out.println(ss.size());
+
         ArrayList<ArrayList> generalListOfSubgraf = new ArrayList<>();      //Массив, куда складываются рабочие подграфы
         ArrayList<TreeSet> generalListOfNodesOfSubgraf = new ArrayList<>(); //Массив рабочих массивов точек
-        ArrayList<Long> firstSubgraf = new ArrayList<>();
-        TreeSet<Long> firstSetOfNodes = new TreeSet<>();
+        ArrayList<OsmWay> firstSubgraf = new ArrayList<>();
+        //TreeSet<Long> firstSetOfNodes = new TreeSet<>();
 
         long[] longs1;
         TreeSet<Long> tmp1 = new TreeSet<>();    // новый set для точек обрабатываемого вея
         longs1 = ss.get(0).getNodeIds();         // вытаскиваем в массив id точек 1-го вея
         for (long lon: longs1) tmp1.add(lon);    // каждую точку добавляем в set
-        generalListOfNodesOfSubgraf.add(tmp1);   // сохраняем set в массив сетов
 
-        for (OsmWay osmWay: ss){
+        generalListOfNodesOfSubgraf.add(tmp1);   // сохраняем set точек первого вея в массив сетов
+        firstSubgraf.add(ss.get(0));            // сохраняем вей в первый субграф
+        generalListOfSubgraf.add(firstSubgraf); // сохраняем первый субграф в массив субграфов
+
+        ArrayList<Integer> tempListForMergeSubgraf = new ArrayList<>(); //массивы для индексов объединямых субграфов и их точек
+        ArrayList<Integer> tempListForMergeSubgrafNodes = new ArrayList<>();
+
+        int index = 0;
+
+        for (OsmWay osmWay: ss){              // берем попорядку веи из массива
+            tempListForMergeSubgraf.clear();
+            tempListForMergeSubgrafNodes.clear();
             long[] longs;
+
             TreeSet<Long> tmp = new TreeSet<>();    // новый set для точек обрабатываемого вея
             longs = osmWay.getNodeIds();         // вытаскиваем в массив id точек 1-го вея
-            for (TreeSet set: generalListOfNodesOfSubgraf){ // для каждого сета точек
-                boolean i = false;
+            for (TreeSet set: generalListOfNodesOfSubgraf){ // для каждого сета точек, которые уже находятся
+               // boolean i = false;
+                index++;
                 for (long Lo: longs){                     // сравниваем точки
-                    if (set.contains(Lo)) i = true;
-                }
-                if (i){                                        // если точка найдена
-                    for (long Lo: longs){
-                        set.add(Lo);
-                    }
+                    //if (set.contains(Lo)) i = true;
 
+                    if (set.contains(Lo)) {
+                        tempListForMergeSubgraf.add(generalListOfNodesOfSubgraf.indexOf(set));  // если точка найдена добавляем индекс сета в массив для объединения
+                        tempListForMergeSubgrafNodes.add(generalListOfNodesOfSubgraf.indexOf(set));
+                    }
                 }
             }
+            // тут надо объединить субграфы
+            if (!tempListForMergeSubgraf.isEmpty()){
+
+            }
+            // или, если нечего объединять, добавить новый субграф
+            else {
+                longs = osmWay.getNodeIds();
+                for (long LL: longs) {
+                   // System.out.println(LL);
+                    tmp.add(LL);
+                }
+
+                generalListOfNodesOfSubgraf.add(tmp);
+                ArrayList<OsmWay> tmpSubgraf = new ArrayList<>();
+                //System.out.println(osmWay.getObjectID());
+
+                tmpSubgraf.add(osmWay);
+
+                generalListOfSubgraf.add(tmpSubgraf);
+            }
+
+
 
         }
+        System.out.println("размер generalListOfSubgraf = " + generalListOfSubgraf.size());
+        System.out.println("размер generalListOfNodesOfSubgraf = " + generalListOfNodesOfSubgraf.size());
+        for (ArrayList<OsmWay> arrayLists: generalListOfSubgraf){
+
+            for (OsmWay osmWays: arrayLists ){
+                System.out.println(osmWays.getObjectID());
+            }
+        }
+        System.out.println(index);
+
 // алгоритм поиска подграфов: елсли найден подграф с точкой, заносим его индекс из Ареейлиста в массив и ищем дальше.
         // если сли найден еще один подграф с точкой заносим его индекс в массив и т.д
         // объединяем найденные подграфы в один и удаляем ненужные
